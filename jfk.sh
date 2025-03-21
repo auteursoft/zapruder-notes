a8135b3d8e7ff4033a404b5e92b95c5b649fcb40
Sean P. Goggins <s@goggins.com>
2025-03-21 14:17:24 -0500

    next tranche of files
    Signed-off-by: Sean P. Goggins <s@goggins.com>

--- jfk.sh
+++ jfk.sh
@@ -2,7 +2,9 @@
 mkdir -p jfk_pdfs
 lynx -dump -listonly "https://www.archives.gov/research/jfk/release-2025" | grep '\.pdf' | awk '{print $2}' > pdf_links.txt
 wget -i pdf_links.txt -P jfk_pdfs/
+
 #Make it executable: 
-#chmod +x download_jfk_pdfs.sh
+#chmod +x jfk.sh
+
 #Run it!!
-#./download_jfk_pdfs.sh
+#./jfk.sh
